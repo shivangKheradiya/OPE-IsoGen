cb3a7cda63ecaeb6aa58b6da4ad218d2b368ce20
Shivang Kheradiya <31017420+shivangKheradiya@users.noreply.github.com>
2026-03-11 17:23:50 +0000

    PyOCC added

--- src/OPE_IsoGen/config_loader.py
+++ src/OPE_IsoGen/config_loader.py
@@ -1,5 +1,5 @@
 import os
-import importlib
+import importlib.util
 import glob
 
 CONFIG_DEFAULT = "symbol_paths.txt"
--- test.bat
+++ test.bat
@@ -3,3 +3,7 @@ conda env remove -n opeisogen-dev
 conda activate opeisogen-dev
 
 pip install -e .
+
+opeisogen run --outfile test.svg
+
+opeisogen run --config symbol_paths.txt --outfile custom.svg
