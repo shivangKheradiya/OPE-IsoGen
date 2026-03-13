7e8c0eedbf732137d71bfc31ad7c4afc5f07af75
Shivang Kheradiya <31017420+shivangKheradiya@users.noreply.github.com>
2026-03-13 15:53:33 +0000

    ISO Functionality needs to impliment

--- src/OPE_IsoGen/occt_core/pipe.py
+++ src/OPE_IsoGen/occt_core/pipe.py
@@ -1,10 +1,18 @@
 # src/OPE_IsoGen/occt_core/pipe.py
 
-from OCC.Core.gp import gp_Pnt, gp_XYZ
+from OCC.Core.gp import gp_Pnt, gp_XYZ, gp_Trsf2d
 from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeEdge
+from .projector import *
+import click
 
-def build_pipe_edge(S, E):
+def build_pipe_edge(S:gp_Pnt, E:gp_Pnt, Projection:str):
+    click.echo(Projection)
+    if Projection == "ÏSO":
+        trsf = gp_iso_transform()
+        S = gp_Pnt(*S).Transformed(trsf)
+        E = gp_Pnt(*E).Transformed(trsf)
+
     return BRepBuilderAPI_MakeEdge(
-        gp_Pnt(gp_XYZ(S[0],S[1],0)),
-        gp_Pnt(gp_XYZ(E[0],E[1],0))
+        gp_Pnt(*S),
+        gp_Pnt(*E)
     ).Edge()
--- src/OPE_IsoGen/occt_core/projector.py
+++ src/OPE_IsoGen/occt_core/projector.py
@@ -14,3 +14,18 @@ def iso_project_point(x, y, z):
     sx = x * E_X[0] + y * E_Y[0] + z * E_Z[0] 
     sy = x * E_X[1] + y * E_Y[1] + z * E_Z[1]
     return sx, sy
+
+def gp_iso_transform():
+    from OCC.Core.gp import gp_Pnt, gp_XYZ, gp_Trsf2d, gp_Trsf, gp_Dir, gp_Ax1
+    trsf = gp_Trsf()
+
+    # Isometric rotations
+    alpha = math.radians(35.264)  # rotate around X
+    beta = math.radians(45)       # rotate around Z
+
+    trsf.SetRotation(gp_Ax1(gp_Pnt(0,0,0), gp_Dir(1,0,0)), alpha)
+    trsf.Multiply(
+        gp_Trsf().SetRotation(gp_Ax1(gp_Pnt(0,0,0), gp_Dir(0,0,1)), beta)
+    )
+
+    return trsf
